275e257cf57a94c20a878857f7f00a42a4cfb046
DeazStar <deadfromtheborn@gmail.com>
2022-11-30 09:53:38 -0700

    enclose using `

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,10 +3,10 @@
 --give it a password user_0d_2_pwd
 --grant it select permission only on hbtn_0d_2 database
 CREATE DATABASE
-    IF NOT EXISTS hbtn_0d_2;
+    IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-   ON hbtn_0d_2.*
+   ON `hbtn_0d_2`.*
    TO 'user_0d_2'@'localhost';
