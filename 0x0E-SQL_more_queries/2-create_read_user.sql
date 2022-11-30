a1e262a5ef986146bae58d9051f44052db42781d
DeazStar <deadfromtheborn@gmail.com>
2022-11-30 11:35:43 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,8 @@
---create a database hbtn_0d_2
---create a user user_0d_2 on local host
---give it a password user_0d_2_pwd
---grant it select permission only on hbtn_0d_2 database
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates a database
+-- create a user
+-- grant SELECT privileges to a user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost 
     IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
