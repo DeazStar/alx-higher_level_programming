6bf28c20d1c47deccc9d82ca9be8e373a7bd9b05
DeazStar <deadfromtheborn@gmail.com>
2022-11-30 09:51:34 -0700

    create a comment

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,7 @@
+--create a database hbtn_0d_2
+--create a user user_0d_2 on local host
+--give it a password user_0d_2_pwd
+--grant it select permission only on hbtn_0d_2 database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
