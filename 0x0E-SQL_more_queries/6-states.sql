b1518f215560b5cfebfae259ddebc42d32101533
DeazStar <deadfromtheborn@gmail.com>
2022-11-30 10:30:43 -0700

    fix bugs

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,10 +3,9 @@
 -- coloumns id, name 
 -- id can not be null, unique , auto-generated and primary key
 -- name can not be null
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
-USE hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS states(
-    id INT UNIQUE AUTO_INCREMENT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    PRIMARY KEY(id),
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
 );
