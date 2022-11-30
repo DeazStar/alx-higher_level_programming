66e030a16ab9517668e533cc7b68531e7ed15303
DeazStar <deadfromtheborn@gmail.com>
2022-11-30 09:52:41 -0700

    fix bug

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,9 +2,11 @@
 --create a user user_0d_2 on local host
 --give it a password user_0d_2_pwd
 --grant it select permission only on hbtn_0d_2 database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+CREATE DATABASE
+    IF NOT EXISTS hbtn_0d_2;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT 
-    ON 'hbtn_0d_2'.*
-    TO 'user_0d_2'@'localhost';
+GRANT SELECT
+   ON hbtn_0d_2.*
+   TO 'user_0d_2'@'localhost';
